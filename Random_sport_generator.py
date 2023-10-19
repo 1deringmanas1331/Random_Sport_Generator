5ada26f1e861da9e0527c08032fd9baf67c25490
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-19 22:29:22 +0530

    Add files via upload

---
+++ Random_sport_generator.py
@@ -0,0 +1,93 @@
+import random
+
+sports_and_games = [
+    {
+        "name": "Soccer",
+        "description": "A team sport played with a spherical ball between two teams of 11 players each.",
+    },
+    {
+        "name": "Basketball",
+        "description": "A team sport played with a ball, where two teams try to score points by shooting the ball through the opponent's basket.",
+    },
+    {
+        "name": "Tennis",
+        "description": "A racquet sport played individually or in pairs, where the goal is to hit the ball over the net and into the opponent's court.",
+    },
+    {
+        "name": "Chess",
+        "description": "A two-player strategy board game played on an 8x8 grid, focusing on the movement of different pieces and the goal of checkmating the opponent's king.",
+    },
+    {
+        "name": "Swimming",
+        "description": "A water sport where participants race through a pool or open water using various swimming strokes.",
+    },
+    {
+        "name": "Table Tennis",
+        "description": "A fast-paced indoor sport played with a small ball and a table divided by a net, involving quick reflexes and skill.",
+    },
+    {
+        "name": "Baseball",
+        "description":"A bat-and-ball game played between two teams, where the batting team attempts to score runs by hitting a pitched ball and running around bases." ,
+    },
+     {
+        "name":"Golf" ,
+        "description":"A precision club-and-ball sport played on a course with the goal of completing each hole with the fewest strokes." ,
+    },
+     {
+        "name":"Volleyball" ,
+        "description":"A team sport played with a ball over a net, where teams try to score points by grounding the ball on the opponent's side of the court." ,
+    },
+     {
+        "name":"Badminton" ,
+        "description": "A racquet sport played indoors or outdoors, focusing on hitting a shuttlecock over the net into the opponent's court.",
+    },
+     {
+        "name":"Rugby" ,
+        "description": "A team sport played with an oval ball, similar to American football, where the objective is to carry or kick the ball over the opponent's goal line." ,
+    },
+     {
+        "name":"Ice Hockey" ,
+        "description": "A team sport played on ice, where players use sticks to shoot a puck into the opponent's goal.",
+    },
+     {
+        "name":"Cricket" ,
+        "description":"A bat-and-ball game played between two teams, where the batting team attempts to score runs and the bowling team tries to dismiss the batsmen." ,
+    },
+     {
+        "name":"Gymnastics",
+        "description":"A sport that involves a variety of physical exercises and routines, often performed on apparatus, showcasing strength, flexibility, and balance." ,
+    },
+     {
+        "name":"Martial Arts" ,
+        "description":  "A broad term for various combat practices, such as karate, judo, taekwondo, and more, focusing on self-defense and discipline.",
+    },
+     {
+        "name":"Cycling" ,
+        "description": "A sport involving riding bicycles, including road racing, track cycling, and mountain biking.",
+    },
+     {
+        "name":"Skiing" ,
+        "description":"A winter sport that involves sliding on snow using skis, with variations like downhill skiing and cross-country skiing." ,
+    },
+     {
+        "name":"Surfing" ,
+        "description":"A water sport in which participants ride ocean waves on a surfboard." ,
+    },
+     {
+        "name":"Fencing" ,
+        "description": "A combat sport that involves two opponents trying to touch each other with a foil, épée, or saber." ,
+    },
+     {
+        "name":"Boxing" ,
+        "description": "A combat sport involving two fighters who punch each other with the goal of scoring points or knocking out the opponent.",
+    },
+]
+
+def generate_random_sport_or_game():
+    sport_or_game = random.choice(sports_and_games)
+    return sport_or_game
+
+if __name__ == "__main__":
+    random_sport = generate_random_sport_or_game()
+    print(f"Sport/Game: {random_sport['name']}")
+    print(f"Description: {random_sport['description']}")
